79c9597571c240869d092eee50d7d33d7b777c30
Weihsueh Chiu <weihsueh.chiu@gmail.com>
2020-08-02 16:45:45 -0500

    Updated scenarios and figures

--- model/SEIR.scenarios.model.R.c
+++ model/SEIR.scenarios.model.R.c
@@ -3,7 +3,7 @@
 
    Model File:  model/SEIR.scenarios.model.R
 
-   Date:  Tue Jul 28 12:04:22 2020
+   Date:  Sun Aug  2 10:53:03 2020
 
    Created by:  "MCSim/mod.exe v6.1.0"
     -- a model preprocessor by Don Maszle
